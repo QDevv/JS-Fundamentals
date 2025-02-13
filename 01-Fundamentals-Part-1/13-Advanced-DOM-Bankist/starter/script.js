8c4b1d198d85dcc41c8976898b05e0a0c3857016
QDevv <abdulkudus333@gmail.com>
2025-02-13 23:47:54 +0100

    DOM

--- 01-Fundamentals-Part-1/13-Advanced-DOM-Bankist/starter/script.js
+++ 01-Fundamentals-Part-1/13-Advanced-DOM-Bankist/starter/script.js
@@ -18,14 +18,19 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
-
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
+
+// NOTES
+// FOREACH METOD CAN BE CALLED ON A NODELIST
