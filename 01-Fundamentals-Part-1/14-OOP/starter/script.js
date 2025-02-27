7d48083e99e2c26e07b563a5eb2cce111d890f7f
QDevv <abdulkudus333@gmail.com>
2025-02-27 23:37:38 +0100

    protoype

--- 01-Fundamentals-Part-1/14-OOP/starter/script.js
+++ 01-Fundamentals-Part-1/14-OOP/starter/script.js
@@ -2,6 +2,12 @@
 const Person = function (name, birthyear) {
   this.name = name;
   this.birthyear = birthyear;
+
+  // NEVER DO THIS
+
+  // calcAge = function () {
+  //   console.log(2037 - birthyear);
+  // };
 };
 
 const james = new Person('james', 1996);
@@ -19,3 +25,25 @@ const matilda = new Person('matilda', 1999);
 const joe = new Person('joe', 1985);
 
 console.log(joe, matilda);
+
+console.log(matilda instanceof Person);
+
+console.log(joe instanceof Person);
+
+// PROTOTYPE
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthyear);
+};
+
+james.calcAge();
+
+//calcage function could be called on James obj due to prototypal inheritance
+console.log(james.__proto__);
+console.log(Person.prototype.isPrototypeOf(matilda));
+
+//prototypeOfLinkedProperties
+console.log(james.hasOwnProperty('name'));
+
+console.log(typeof [2, 4]);
