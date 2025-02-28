5142b5a63bda1b4b0fd0565cab4a73b572f04658
QDevv <abdulkudus333@gmail.com>
2025-02-28 23:52:09 +0100

    coding challenge

--- 01-Fundamentals-Part-1/14-OOP/starter/script.js
+++ 01-Fundamentals-Part-1/14-OOP/starter/script.js
@@ -47,3 +47,29 @@ console.log(Person.prototype.isPrototypeOf(matilda));
 console.log(james.hasOwnProperty('name'));
 
 console.log(typeof [2, 4]);
+
+// coding challenge //
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+const BMW = new Car('BMW', 120);
+const Mercedes = new Car('Mercedes', 95);
+
+BMW.accelerate();
+BMW.brake();
+BMW.accelerate();
+
+Mercedes.accelerate();
+Mercedes.brake();
